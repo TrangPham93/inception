d5b8f50f5b72bbcff74cff908517a7115962d5ea
TrangPham93 <hatrangc2@gmail.com>
2026-02-02 15:07:56 +0200

    update .env and docker-compose.yml

--- srcs/requirements/wordpress/data/wp-config.php
+++ srcs/requirements/wordpress/data/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+define( 'DB_NAME', 'wordpress_db');
 
 /** MySQL database username */
-define( 'DB_USER', 'wp_user');
+define( 'DB_USER', 'trpham');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'my_secure_wordpress_password');
+define( 'DB_PASSWORD', 'trpham');
 
 /** MySQL hostname */
 define( 'DB_HOST', 'db:3306');
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '3e42d6141df7b7d6271e44fd8e8c8d2c7f58cfe1');
-define( 'SECURE_AUTH_KEY',  'ee12958e9f304e8a7a1a6df8039335cbed361f9e');
-define( 'LOGGED_IN_KEY',    '33ab6f1684cdc25b52aefc7c2070e7deea9343a1');
-define( 'NONCE_KEY',        'edf9a29225f347fcf062bd05e327a9f024d74281');
-define( 'AUTH_SALT',        '428869e747c8ae47005da357200e863f6bf99c7f');
-define( 'SECURE_AUTH_SALT', '71874de35c764e2c2c7eb1d45795f5ae7058f34d');
-define( 'LOGGED_IN_SALT',   '15668288ecb4abdd20aa6b50f2237b833280b7b5');
-define( 'NONCE_SALT',       '957e2c73740e358b0f3623a6de4d0bafd6cd5d0e');
+define( 'AUTH_KEY',         '66c48ffb03161cd37c00cdc81259ba46156c8ac8');
+define( 'SECURE_AUTH_KEY',  '0e61ad58252a387de0f1bf39c861e32c9d1b4b2b');
+define( 'LOGGED_IN_KEY',    'fd3eb4328ff7656617f401e0672164f736097f79');
+define( 'NONCE_KEY',        '4e3a745f9123b6995e9f03d7c4d5299746cd44a1');
+define( 'AUTH_SALT',        '5daa65d004f1cfe1b6ca5e0c41ab76bac334b522');
+define( 'SECURE_AUTH_SALT', '7e4c97655e839ef4df68f910f4b93918e18b3d78');
+define( 'LOGGED_IN_SALT',   '8314716043417a632ab6d2095dd5a293c8bc8a92');
+define( 'NONCE_SALT',       'dceb6f122ea7962be6b8ab3b17218ea57c8d42aa');
 
 /**#@-*/
 

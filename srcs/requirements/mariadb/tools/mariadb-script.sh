46320b0b6bda1f91de2bed5c7df0f8a497aada05
TrangPham93 <hatrangc2@gmail.com>
2026-02-07 21:48:15 +0200

    customize nginx and mariadb

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -3,30 +3,38 @@
 # make the script stop immediately if any cmd fails
 set -e
 
+chmod -R 755 /var/lib/mysql
+
+mkdir -p /run/mysqld
+
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
+
 # Initialize database if empty
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "Initializing MariaDB database..."
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-
-	# mariadb-safe is a wrapper with extra safety features that restarts mariadbd if mariadbd crashes
-	# start mariadb temporarily and save the processID of the mariadb background 
-	mariadbd-safe --datadir=/var/lib/mysql &
-	pid="$!"
-
-	# wait for mariadb to start
-	sleep 5
+	mariadb-install-db --basedir=/usr --user=mysql --datadir=/var/lib/mysql
 
 	# set up root password and create database/user
-	mariadb -e "CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DATABASE_NAME};"
-	mariadb -e "CREATE USER IF NOT EXISTS '${WORDPRESS_DATABASE_USER}' IDENTIFIED BY '${WORDPRESS_DATABASE_USER_PASSWORD}';"
-	mariadb -e "GRANT ALL PRIVILEGES ON ${WORDPRESS_DATABASE_NAME}.* TO '${WORDPRESS_DATABASE_USER}'@'%';"
-	mariadb -e "FLUSH PRIVILEGES;"
+	echo "==> Creating WordPress database and user..."
+	# pipe SQL into MariaDB during bootstrap.
+	mysqld --user=mysql --bootstrap << EOF
+USE mysql;
+FLUSH PRIVILEGES;
 
-	#stop temporary server
-	kill "$pid"
-	wait "$pid"
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+
+CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DATABASE_NAME};
+CREATE USER IF NOT EXISTS ${WORDPRESS_DATABASE_USER}@'%' IDENTIFIED BY '${WORDPRESS_DATABASE_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${WORDPRESS_DATABASE_NAME}.* TO ${WORDPRESS_DATABASE_USER}@'%';
+FLUSH PRIVILEGES;
+EOF
+
+else
+    echo "==> MariaDB is already installed. Database and users are configured."
 fi
 
 echo "Starting MariaDB..."
 # --console outputs logs to stdout so Docker can show logs
 exec mariadbd --user=mysql --console
+### ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';

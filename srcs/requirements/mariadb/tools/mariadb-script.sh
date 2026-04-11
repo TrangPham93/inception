2bceaefbb332bd67cb2bcadbb21ddffb9c743b57
Trang Pham <trpham@c1r1p2.hive.fi>
2026-04-11 15:54:39 +0300

    fix mariadb

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -11,22 +11,14 @@ INIT_FILE="${DATADIR}/init.sql"
 mkdir -p "${DATADIR}" "${RUNDIR}" "${LOGDIR}"
 chown -R mysql:mysql "${DATADIR}" "${RUNDIR}" "${LOGDIR}"
 
-# chmod 750 /var/lib/mysql
-# chown -R mysql:mysql /var/lib/mysql /run/mysqld
-
 # Initialize database if empty
 if [ ! -d "${DATADIR}/mysql" ]; then
 	echo "Initializing MariaDB database..."
-	mariadb-install-db --basedir=/usr --user=mysql --datadir="${DATADIR} --skip-test-db"
+	mariadb-install-db --basedir=/usr --user=mysql --datadir="${DATADIR}" --skip-test-db
 fi
-	# # set up root password and create database/user
-	# echo "==> Creating WordPress database and user..."
-	# # pipe SQL into MariaDB during bootstrap.
-	# mysqld --user=mysql --bootstrap << EOF
 
 USE mysql;
 
-# FLUSH PRIVILEGES;
 echo "Preparing init SQL"
 cat > "${INIT_FILE}" << EOF
 
@@ -41,10 +33,6 @@ chown mysql:mysql "${INIT_FILE}"
 
 echo "Starting MariaDB..."
 
-# sleep 5
-# --console outputs logs to stdout so Docker can show logs
-# exec mariadbd --user=mysql --console
-### ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 exec mariadbd --defaults-file="${CONFIG_FILE}" --init-file="${INIT_FILE}" --console
 
 
